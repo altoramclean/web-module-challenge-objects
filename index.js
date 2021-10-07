de02d01df76c14c5e6398ffbf2f15e7ebdec65aa
Altora Mclean <altoramclean@gmail.com>
2021-10-07 16:10:30 -0400

    just a few more changes

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   2. log the reviews array to the console to check your work
 */
 
- 
+reviews[7].feedback =  "this place is chill with really cool people, great for getting work done on weekdays";
 
 
 
@@ -127,9 +127,11 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
-}
+function getReviewByIndex(array, num) {
+
+  return `${array[num].name} gave the restaurant a ${array[num].rating} star review, and their feedback was: ${array[num].feedback}`;
+  
+  }
 
 
   
@@ -146,10 +148,11 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(array) {
+  return `${array[array.length -1].name} gave the restaurant a ${array[array.length -1].rating
+  } star review, and their feedback was: ${array[array.length -1].feedback}`;
 } 
-
+console.log('Task 7:', getLastReview(reviews));
 
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
